3eca5c00f437bb25afc5a4efa9c78fe68ba1a5d5
Vladimir <vladimir.d.babichev@gmail.com>
2020-05-12 15:40:39 +0100

    Removed backend and state configuration from module

--- remote-state/main.tf
+++
@@ -1,9 +0,0 @@
-provider "google" {
-  project     = var.gcp_project_id
-  credentials = var.gcp_credentials
-}
-
-resource "google_storage_bucket" "imagestore" {
-  name          = "pd-tf-state-${var.deployment_name}"
-  force_destroy = true
-}
--- variables.tf
+++ variables.tf
@@ -9,12 +9,6 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "deployment_name" {
-  description = "Name of this polkadot deployment"
-  default     = "polkadot"
-  type        = string
-}
-
 variable "location" {
   description = "Digital Ocean region"
   type        = string
@@ -34,16 +28,6 @@ variable "machine_type" {
 
 variable "k8s_version" {
   description = "Kubernetes version to use for the cluster"
-  default     = "1.14.3-do.0"
-  type        = string
-}
-
-variable "gcp_project_id" {
-  description = "Google cloud project id used for terraform state"
-  type        = string
-}
-
-variable "gcp_credentials" {
-  description = "Either the path to or the contents of a GCP service account key file in JSON format"
+  default     = "1.15.11-do.0"
   type        = string
 }
