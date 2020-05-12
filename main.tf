c47b36a0fa99a202556519c6045f82bdeddc4dc4
Vladimir <vladimir.d.babichev@gmail.com>
2020-05-12 15:49:13 +0100

    Fixed formatting

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "digitalocean_kubernetes_cluster" "primary" {
-  name    = var.cluster_name
-  region  = var.location
-  version = var.k8s_version
-  provider = "digitalocean"
+  name     = var.cluster_name
+  region   = var.location
+  version  = var.k8s_version
+  provider = digitalocean
 
   node_pool {
     name       = "node-pool"
