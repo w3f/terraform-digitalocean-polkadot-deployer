e2db923ed66cf6ec3e4deccf3ff3f71786c64245
Vladimir <vladimir.d.babichev@gmail.com>
2020-05-14 23:29:23 +0100

    Updated fixtures and moved test configuration into a separate file

--- test/cluster_test.go
+++ test/cluster_test.go
@@ -1,16 +1,8 @@
 package test
 
 import (
-    "fmt"
-    "os"
-    "strings"
     "testing"
-
-    "github.com/gruntwork-io/terratest/modules/random"
-    "github.com/gruntwork-io/terratest/modules/terraform"
     "github.com/gruntwork-io/terratest/modules/test-structure"
-    "github.com/stretchr/testify/require"
-
     test_polkadot "github.com/vladimir-babichev/terratest-polkadot-deployer"
 )
 
@@ -21,30 +13,8 @@ func TestTerraformClusterCreation(t *testing.T) {
 
     // Test configuration
     test_structure.RunTestStage(t, "create_terratest_options", func() {
-        nodeCount := 2
-        nodePort := 30100
-        uniqueID := strings.ToLower(random.UniqueId())
-        clusterName := fmt.Sprintf("test-polkadot-%s", uniqueID)
-        doToken := os.Getenv("DIGITALOCEAN_TOKEN")
-        require.NotEmpty(t, doToken, "DIGITALOCEAN_TOKEN variable is not set")
-
-        terraformOptions := &terraform.Options{
-            TerraformDir: terraformDir,
-            Vars: map[string]interface{} {
-                "cluster_name": clusterName,
-                "do_token":     doToken,
-                "location":     "lon1",
-                "machine_type": "s-1vcpu-2gb",
-                "node_count":   nodeCount,
-            },
-            NoColor: true,
-        }
-
-        test_structure.SaveInt(t, terraformDir, "nodeCount", nodeCount)
-        test_structure.SaveInt(t, terraformDir, "nodePort", nodePort)
-        test_structure.SaveString(t, terraformDir, "clusterName", clusterName)
-        test_structure.SaveString(t, terraformDir, "uniqueID", uniqueID)
-        test_structure.SaveTerraformOptions(t, terraformDir, terraformOptions)
+        createTerraformOptions(t, terraformDir)
+        createHelmOptions(t, terraformDir)
     })
 
     // At the end of the test, run `terraform destroy` to clean up any resources that were created
@@ -64,11 +34,16 @@ func TestTerraformClusterCreation(t *testing.T) {
 
     // Validate cluster size
     test_structure.RunTestStage(t, "validate_node_count", func() {
-        test_polkadot.ValidateNodeCount(t, terraformDir)
+        test_polkadot.TestNodeCount(t, terraformDir)
     })
 
     // Validate external connectivity to the service
     test_structure.RunTestStage(t, "validate_service", func() {
-        test_polkadot.ValidateServiceAvailability(t, terraformDir)
+        test_polkadot.TestServiceAvailability(t, terraformDir)
+    })
+
+    // Validate no resources will change on subsequent terraform executions
+    test_structure.RunTestStage(t, "validate_plan", func() {
+        test_polkadot.TestResourceChanges(t, terraformDir)
     })
 }
--- test/go.mod
+++ test/go.mod
@@ -1,9 +1,9 @@
-module github.com/w3f/terraform-digitalocean-polkadot-deployer
+module github.com/w3f/terraform-digitalocean-polkadot-deployer/test
 
 go 1.14
 
 require (
 	github.com/gruntwork-io/terratest v0.27.2
 	github.com/stretchr/testify v1.5.1
-	github.com/vladimir-babichev/terratest-polkadot-deployer v0.0.0-20200514000955-c9fbb12dbeb5
+	github.com/vladimir-babichev/terratest-polkadot-deployer v0.2.0
 )
